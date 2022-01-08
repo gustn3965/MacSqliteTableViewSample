befe8d780a331b66ae7439e684d6c15331146c1a
gustn3965 <gustn3965@gmail.com>
2022-01-08 22:03:23 +0900

    env: gitignore 추가, 세팅

---
+++ SqliteTableVeiw/Window/MainWindowController.h
@@ -0,0 +1,16 @@
+//
+//  MainWindowController.h
+//  SqliteTableVeiw
+//
+//  Created by hyunsu on 2022/01/08.
+//
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainWindowController : NSWindowController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SqliteTableVeiw/Window/MainWindowController.m
@@ -0,0 +1,22 @@
+//
+//  MainWindowController.m
+//  SqliteTableVeiw
+//
+//  Created by hyunsu on 2022/01/08.
+//
+
+#import "MainWindowController.h"
+
+@interface MainWindowController ()
+
+@end
+
+@implementation MainWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
