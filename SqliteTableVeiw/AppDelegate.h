621a8fa1053a029e378be6e19dcfa3c1ddf07817
gustn3965 <gustn3965@gmail.com>
2022-01-08 20:36:46 +0900

    Initial Commit

---
+++ SqliteTableVeiw/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SqliteTableVeiw
+//
+//  Created by hyunsu on 2022/01/08.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SqliteTableVeiw/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SqliteTableVeiw
+//
+//  Created by hyunsu on 2022/01/08.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
